394a68467ad7ac85f295ad4200cb7d70bb1a148f
Noah Robert Franz <nfranz@r5u20n1.puma.hpc.arizona.edu>
2023-10-30 14:52:13 -0700

    rearrange

--- ASTR513/Project/run.sh
+++ ASTR513/Project/run.sh
@@ -1,5 +1,10 @@
 for name in $(cat data/names.txt);
 do
-    PATH=data/hostdata/$name
-    sbatch run_prospector.slurm $PATH
+    INPATH=data/hostdata/$name
+    FILE=$(basename -- $INPATH)
+    OUTFILENAME="${FILE%.*}.h5"
+    OUTPATH="data/out/${OUTFILENAME}"
+    
+    echo $INPATH $OUTPATH
+    sbatch run_prospector.slurm $INPATH $OUTPATH
 done
